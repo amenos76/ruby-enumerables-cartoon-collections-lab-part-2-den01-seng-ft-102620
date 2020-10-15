b7fc3f52ad8e3a88f670cd82a4fb538408ae4342
Augie Menos <augie.menos@gmail.com>
2020-10-15 20:53:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,6 +5,11 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |string|
+    "#{string.toUpperCase}!" 
+  end
+
+
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
 end
